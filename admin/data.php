6a7cec5d9ed0aa001844db3a548fd56dbcbec698
suryadika007@gmail.com <suryadika007@gmail.com>
2023-05-04 15:17:47 +0700

    first commit

---
+++ admin/data.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+if (isset($_SESSION['logged']) && !empty($_SESSION['logged'])) {
+	include "../database.php";
+	include '_header.php';
+?>
+
+	<div class="container">
+		<h2>Data Kelulusan</h2>
+		<hr>
+		<div class="row col-sm-8">
+			<form class="form-horizontal well" method="post" action="data_upload.php" enctype="multipart/form-data">
+				<div class="form-group">
+					<label for="importCsv" class="col-sm-3 control-label">CSV/Excel File</label>
+					<div class="col-sm-9">
+						<div class="fileinput fileinput-new input-group" data-provides="fileinput">
+							<div class="form-control" data-trigger="fileinput">
+								<i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span>
+							</div>
+							<span class="input-group-addon btn btn-default btn-file">
+								<span class="fileinput-new">Pilih file</span>
+								<span class="fileinput-exists">Ganti</span>
+								<input type="file" name="file">
+							</span>
+							<a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Buang</a>
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-offset-3 col-sm-9">
+						<button type="submit" name="submit" class="btn btn-default">Upload</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="container">
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th rowspan="2" style="text-align:center">No</th>
+						<th rowspan="2" style="text-align:center">NIS</th>
+						<th rowspan="2" style="text-align:center">NISN</th>
+						<th rowspan="2" style="text-align:center">Nama Siswa</th>
+						<th rowspan="2" style="text-align:center">Kelas</th>
+						<th rowspan="2" style="text-align:center">Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$batas = 50;
+					$halaman = isset($_GET['halaman']) ? (int)$_GET['halaman'] : 1;
+					$halaman_awal = ($halaman > 1) ? ($halaman * $batas) - $batas : 0;
+
+					$previous = $halaman - 1;
+					$next = $halaman + 1;
+
+					$qsiswa = mysqli_query($db_conn, "SELECT * FROM un_siswa ORDER BY jur,nama");
+					$jumlah_data = mysqli_num_rows($qsiswa);
+					$total_halaman = ceil($jumlah_data / $batas);
+
+
+					$data_qsiswa = mysqli_query($db_conn, "SELECT * FROM un_siswa ORDER BY jur,nama limit $halaman_awal, $batas");
+					$no = $halaman_awal + 1;
+
+
+					if (mysqli_num_rows($data_qsiswa) > 0) {
+						while ($data = mysqli_fetch_array($data_qsiswa)) {
+							echo '<tr>';
+							echo '<td>' . $no++;
+							echo '<td>' . $data['no_nis'] . '</td>';
+							echo '<td>' . $data['no_nisn'] . '</td>';
+							echo '<td>' . $data['nama'] . '</td>';
+							echo '<td style="text-align:center">' . $data['jur'] . '</td>';
+							echo '<td style="text-align:center">';
+							echo ($data['status'] == 1) ? '<b>Lulus<b>' : '<em>Tidak Lulus</em>';
+							echo '</td>';
+							echo '</tr>';
+						}
+					} else {
+						echo '<tr><td colspan="8"><em>Belum ada data! Segera lakukan upload data.</em></td></tr>';
+					}
+					?>
+				</tbody>
+			</table>
+			<nav>
+				<ul class="pagination justify-content-center">
+					<li class="page-item">
+						<a class="page-link" <?php if ($halaman > 1) {
+													echo "href='?halaman=$previous'";
+												} ?>>Previous</a>
+					</li>
+					<?php
+					for ($x = 1; $x <= $total_halaman; $x++) {
+					?>
+						<li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+					<?php
+					}
+					?>
+					<li class="page-item">
+						<a class="page-link" <?php if ($halaman < $total_halaman) {
+													echo "href='?halaman=$next'";
+												} ?>>Next</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+<?php
+	include '_footer.php';
+} else {
+	header('Location: ./login.php');
+}
+?>
---
+++ index.php
@@ -0,0 +1,142 @@
+<?php
+include "database.php";
+$que = mysqli_query($db_conn, "SELECT * FROM un_konfigurasi");
+$hsl = mysqli_fetch_array($que);
+$timestamp = strtotime($hsl['tgl_pengumuman']);
+//echo $timestamp;
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="icon" type="image/png" href="img/myicon.png" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="aplikasi sederhana untuk menampilkan pengumuman hasil ujian nasional secara online">
+	<meta name="author" content="slamet.bsan@gmail.com">
+	<title>Pengumuman Kelulusan Siswa</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/jasny-bootstrap.min.css" rel="stylesheet">
+	<link href="css/kelulusan.css" rel="stylesheet">
+</head>
+
+<body>
+	<nav class="navbar navbar-inverse navbar-static-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="./">Pengumuman Kelulusan Siswa</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="./">Home</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</nav>
+
+	<div class="container">
+		<h3>Pengumuman Kelulusan Siswa SMAN 15 Bandung Tahun <?= $hsl['tahun'] ?></h3>
+		<!-- countdown -->
+
+		<div id="clock" class="lead"></div>
+
+
+		<div id="xpengumuman">
+			<?php
+			if (isset($_REQUEST['submit'])) {
+				//tampilkan hasil queri jika ada
+				$no_nisn = $_REQUEST['nomor'];
+
+				$hasil = mysqli_query($db_conn, "SELECT * FROM un_siswa WHERE no_nisn='$no_nisn'");
+				if (mysqli_num_rows($hasil) > 0) {
+					$data = mysqli_fetch_array($hasil);
+
+			?>
+					<table class="table table-bordered">
+						<tr>
+							<td>NISN</td>
+							<td><?php echo $data['no_nisn']; ?></td>
+						</tr>
+						<tr>
+							<td>Nama Siswa</td>
+							<td><?php echo $data['nama']; ?></td>
+						</tr>
+						<tr>
+							<td>Kelas</td>
+							<td><?php echo $data['jur']; ?></td>
+						</tr>
+					</table>
+
+					<?php
+					if ($data['status'] == 1) {
+						echo '<div class="alert alert-success" role="alert"><strong>SELAMAT !</strong> Anda dinyatakan MASUK sebagai Siswa Eligible.</div>';
+						echo '<div class="alert alert-secondary" role="alert">Silahkan Screenshot dan Hubungi BK masing-masing.</div>';
+					} else {
+						echo '<div class="alert alert-danger" role="alert"><strong>MAAF !</strong> Anda dinyatakan TIDAK MASUK sebagai Siswa Eligible. TETAP SEMANGAT!.</div>';
+					}
+					?>
+
+
+				<?php
+				} else {
+					echo '<strong>Nomor NISN</strong> yang anda inputkan tidak ditemukan! periksa kembali <strong>Nomor NISN</strong> anda.';
+					//tampilkan pop-up dan kembali tampilkan form
+				}
+			} else {
+				//tampilkan form input nomor ujian
+				?>
+				<p>Masukkan NISN pada form yang disediakan.</p>
+
+				<form method="post">
+					<div class="input-group">
+						<input type="text" name="nomor" class="form-control" data-mask="9999999999" placeholder="Masukkan NISN" required>
+						<span class="input-group-btn">
+							<button class="btn btn-primary" type="submit" name="submit">Periksa!</button>
+						</span>
+					</div>
+				</form>
+			<?php
+			}
+			?>
+		</div>
+	</div><!-- /.container -->
+
+	<footer class="footer">
+		<div class="container">
+			<p class="text-muted">&copy; <?= $hsl['tahun'] ?> &middot; Tim IT <?= $hsl['sekolah'] ?></p>
+		</div>
+	</footer>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.countdown.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jasny-bootstrap.min.js"></script>
+	<script type="text/javascript">
+		var skrg = Date.now();
+		$('#clock').countdown("<?= $hsl['tgl_pengumuman'] ?>", {
+				elapse: true
+			})
+			.on('update.countdown', function(event) {
+				var $this = $(this);
+				if (event.elapsed) {
+					$("#xpengumuman").show();
+					$("#clock").hide();
+				} else {
+					$this.html(event.strftime('<strong>%H</strong> Jam <strong>%M</strong> Menit <strong>%S</strong> Detik Lagi Menuju Pengumuman Kelulusan'));
+					$("#xpengumuman").hide();
+				}
+			});
+	</script>
+</body>
+
+</html>
