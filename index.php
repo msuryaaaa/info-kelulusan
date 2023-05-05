39722c95019bca8145d25e4cad0791debaead711
suryadika007@gmail.com <suryadika007@gmail.com>
2023-05-05 07:56:02 +0700

    update tampilan index

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $timestamp = strtotime($hsl['tgl_pengumuman']);
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<meta name="description" content="aplikasi sederhana untuk menampilkan pengumuman hasil ujian nasional secara online">
+	<meta name="description" content="aplikasi sederhana untuk menampilkan pengumuman kelulusan dan sebagainya secara online">
 	<meta name="author" content="slamet.bsan@gmail.com">
 	<title>Pengumuman Kelulusan Siswa</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
